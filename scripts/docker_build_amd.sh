f2cfe6b65703bbec061549d5ba40396e9c565a0a
Michael Melesse <micmelesse@gmail.com>
2020-03-06 21:52:10 +0000

    add amd docker file

--- scripts/docker_build_amd.sh
+++ scripts/docker_build_amd.sh
@@ -1,3 +1,3 @@
 cd docker
-wget https://s3.us-east-2.amazonaws.com/dgl.ai/dataset/FB15k.zip -P install/
-docker build -t dgl-cpu -f Dockerfile.ci_cpu .
+# wget https://s3.us-east-2.amazonaws.com/dgl.ai/dataset/FB15k.zip -P install/
+docker build -t dgl-gpu-amd -f Dockerfile.ci_gpu_amd .
