ec9b4a4d32b9da49659e610f04bbd10ad4caa199
Michael Melesse <micmelesse@gmail.com>
2020-03-10 15:45:38 -0500

    update lib addtion

--- scripts/docker_build_nvidia.sh
+++ scripts/docker_build_nvidia.sh
@@ -2,4 +2,4 @@ cd docker
 # wget https://s3.us-east-2.amazonaws.com/dgl.ai/dataset/FB15k.zip -P install/
 # docker build -t dgl-gpu -f Dockerfile.ci_gpu_torch_1.2.0 .
 
-docker build -t dgl-gpu -f Dockerfile.ci_gpu .
+docker build -t dgl-gpu-nvidia -f Dockerfile.ci_gpu .
--- scripts/docker_run_nvidia.sh
+++ scripts/docker_run_nvidia.sh
@@ -1,2 +1,2 @@
 alias nv_drun='sudo docker run -it --network=host --runtime=nvidia --ipc=host -v $HOME/dockerx:/dockerx -w /dockerx/dgl'
-nv_drun dgl-gpu
+nv_drun dgl-gpu-nvidia
